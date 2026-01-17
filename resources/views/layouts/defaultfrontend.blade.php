624fd2cf29b8945d06c282c3c3734a8d504e8463
Christiane Mwenge <christianemwenge@gmail.com>
2026-01-17 22:23:22 +0100

    modification

--- resources/views/layouts/defaultfrontend.blade.php
+++ resources/views/layouts/defaultfrontend.blade.php
@@ -3,13 +3,13 @@
 
 <head>
     <meta charset="utf-8">
-    <title>eLEARNING - eLearning HTML Template</title>
+    <title>CFP Mon Coeur</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
 
-    <!-- Favicon -->
-    <link href="{{asset('assets_frontend/img/favicon.ico')}}" rel="icon">
+    <!-- Favicon Icon -->
+  <link rel="icon" type="image/png" href="{{ asset('assets_backend/images/favicon.png?v=1') }}">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
